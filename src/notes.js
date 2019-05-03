5c90730a158a26beccb9dab573cafe1082021ac9
Jamie Dixon <jamie@jamie-dixon.co.uk>
2019-05-03 16:00:57 +0100

    Add note sounds
    Lots of changes here. Struggled a bit with the logic but finally got there. Needs tidying up.

--- src/notes.js
+++ src/notes.js
@@ -2,41 +2,44 @@ export const generateIntervals = (majorIntervals, mode) => {
   const rightIntervals = majorIntervals.slice(0, mode);
   const leftIntervals = majorIntervals.slice(mode, majorIntervals.length);
   return [...leftIntervals, ...rightIntervals];
-}
+};
 
 export const getNotesForKey = (key, mode, intervals, notes) => {
-const keyIndex = notes.findIndex(note => {
-  const [a, b] = note.split('/');
-  return a === key || b === key;
-});
-
-const rightNotes = notes.slice(0,keyIndex);
-const leftNotes = notes.slice(keyIndex, notes.length);
-const orderedNotes = [...leftNotes, ...rightNotes];
-
-const keyNotes = intervals.reduce((agg, interval) => {
-  const { notes, count } = agg;
-  const nextCount = count + (interval + interval);
-  const note = orderedNotes[nextCount];
-  
-  if (!note) {
-    return notes;
-  }
-
-  const [sharp, flat] = note.split('/');
-  const [noteLetter] = note;
-
-  const noteAlreadyUsed = agg.notes.find(note => {
-    return note[0] === noteLetter;
+  const keyIndex = notes.findIndex(note => {
+    const [a, b] = note.split('/');
+    return a === key || b === key;
   });
 
-  const realNote = noteAlreadyUsed ? flat : sharp;
+  const rightNotes = notes.slice(0, keyIndex);
+  const leftNotes = notes.slice(keyIndex, notes.length);
+  const orderedNotes = [...leftNotes, ...rightNotes];
 
-  return {
-    count: nextCount,
-    notes: [...agg.notes, realNote]
-  }
-}, { count: 0, notes: [key]});
+  const keyNotes = intervals.reduce(
+    (agg, interval) => {
+      const { notes, count } = agg;
+      const nextCount = count + (interval + interval);
+      const note = orderedNotes[nextCount];
 
-return keyNotes;
+      if (!note) {
+        return notes;
+      }
+
+      const [sharp, flat] = note.split('/');
+      const [noteLetter] = note;
+
+      const noteAlreadyUsed = agg.notes.find(note => {
+        return note[0] === noteLetter;
+      });
+
+      const realNote = noteAlreadyUsed ? flat : sharp;
+
+      return {
+        count: nextCount,
+        notes: [...agg.notes, realNote]
+      };
+    },
+    { count: 0, notes: [key] }
+  );
+
+  return keyNotes;
 };
